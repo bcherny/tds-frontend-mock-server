d9b5f16b840695760f93aa730dc93a8cf44b84d0
Boris Cherny <boris@performancejs.com>
2017-10-30 16:08:04 -0700

    add providers mock response

--- index.ts
+++ index.ts
@@ -12,9 +12,25 @@ createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
   let app = express()
 
   app
+  .use(express.json())
   .use(cors())
-    .post('/api/representative_points', (req, res) =>
-      res.send(require('./mocks/respresentative_points.json').map(_ => ({..._, population: random(100, 100000)})))
+    .post('/api/representative_points', (req, res) => {
+
+      if (req.body.serviceAreaIds.length < 1) {
+        return res.send([])
+      }
+
+      res.send(require('./mocks/respresentative_points.json').map(_ => ({
+        ..._,
+        population: random(100, 100000)
+      })))
+    })
+
+    .post('/api/providers', (req, res) =>
+      res.send({
+        successes: require('./mocks/providers.json'),
+        errors: []
+      })
     )
 
   createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(9001)
